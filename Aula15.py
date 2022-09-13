2145c7381469937d7deffac389ef5d5ad3de11ec
Maicon Bitencorte <maicon.bitencorte@gmail.com>
2022-09-13 20:58:32 -0300

    Add files via upload

---
+++ Aula15.py
@@ -0,0 +1,27 @@
+# Laços de repetição (Parte 3)
+# Interrompendo repetições while
+
+enquanto Verdadeiro:
+    se bloco
+        passo
+    se bloco vazio
+        pula
+    se moeda
+        pega
+    se troféu
+        pula
+        interrompa
+pega
+
+
+while True:
+    if bloco
+        passo
+    if bloco vazio
+        pula
+    if moeda
+        pega
+    if troféu
+        pula
+        break
+pega
